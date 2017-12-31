d7658df05b3ad3d143030070484c8ed3be1429c1
Nimish Shikerkar <Shikerkar@IAZI.LOCAL>
2017-12-31 22:09:13 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { Student } from 'app/student.model';
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  name:string;
+  email:string;
+  marks:number;
+
+  private student:Student;
+  
+  private showStudent()
+  {
+    this.student = new Student();
+    this.student.name = this.name;
+    this.student.email = this.email;
+    this.student.marks = this.marks;
+  }
 }
---
+++ src/app/student.model.ts
@@ -0,0 +1,5 @@
+export class Student{
+    name: string;
+    email: string;
+    marks: number;
+}
