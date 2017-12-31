2105d20dcf8d4aeb47de96d318eac462b0e1b98c
Nimish Shikerkar <Shikerkar@IAZI.LOCAL>
2017-12-31 22:34:50 +0530

    Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,9 @@ export class AppComponent {
   email:string;
   marks:number;
 
+  hasData:boolean = false;
+  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+  
   private student:Student;
   
   private showStudent()
@@ -20,5 +23,7 @@ export class AppComponent {
     this.student.name = this.name;
     this.student.email = this.email;
     this.student.marks = this.marks;
+
+    this.hasData = true;
   }
 }
